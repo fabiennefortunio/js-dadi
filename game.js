3418f40ceb91f91ad4ec655288223d24c1220c05
Fabienne <fortunio.fabienne@gmail.com>
2019-11-15 15:47:23 +0100

    variabili

--- game.js
+++ game.js
@@ -1,6 +1,11 @@
-// creare due variabili vince il risultato maggiore di 10..
+/*Generare un numero casuale tra 1 e 6.
+Attraverso un prompt chiedere all'utente un numero tra 1 e 6.
+Stampare un messaggio per dire all'utente se ha vinto oppure no (il numero più alto vince).*/
 
-var num = Math.random();
+/*var num = Math.random();
 var num2 = num * 6;
 var dadi = Math.ceil (num2);
-alert(dadi);
+alert(dadi);*/
+var question = prompt('Entry a number');
+var num = Math.random();
+var num2 = num * 6;
