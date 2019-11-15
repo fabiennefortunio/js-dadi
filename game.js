ec62b2878017231595c5f0edb96157266e38457f
Fabienne <fortunio.fabienne@gmail.com>
2019-11-15 16:11:30 +0100

    funzionalitá finita

--- game.js
+++ game.js
@@ -2,15 +2,12 @@
 Attraverso un prompt chiedere all'utente un numero tra 1 e 6.
 Stampare un messaggio per dire all'utente se ha vinto oppure no (il numero più alto vince).*/
 
-/*var num = Math.random();
-var num2 = num * 6;
-var dadi = Math.ceil (num2);
-alert(dadi);*/
-var num = prompt('Entry a number');
-var num2 = num * 6;
-if num > num2 {
-    console.log (you won)
-    
+var num = prompt("Entry a number");
+var num2 = alert(Math.floor(Math.random() * 6));
+
+if (num > num2){
+    console.log("you won");
+
 } else {
-    console.log(you lose)
+    console.log("you lose");
 }
