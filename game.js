fe43c60f33db2cbfe20ef2aa83983b4cc1194443
Fabienne <fortunio.fabienne@gmail.com>
2019-11-15 17:14:21 +0100

    js

--- game.js
+++ game.js
@@ -2,17 +2,27 @@
 Attraverso un prompt chiedere all'utente un numero tra 1 e 6.
 Stampare un messaggio per dire all'utente se ha vinto oppure no (il numero più alto vince).
 
-Obiettivo: genero numero casuale
-Punto 1:
+Obiettivo: generare numero casuale
+Punto 1: utente inserisce un numero
+Punto 2: il PC genera un numero random
+Punto 3: messaggio in screen "Hai vinto" Hai perso"
 */
 
 var num = prompt("Entry a number");
 var num2 =(Math.floor(Math.random() * 5)+1);
-alert(num2); //come cambiare la scritta della alerta
+//alert(num2); //come cambiare la scritta della alerta
 
 if (num > num2){
-    console.log("you won");
+    console.log("hai vinto");
+    document.getElementById('won');
+    element.setAttribute("class", "visible;");
+    if(isNaN(num)) {
+    // la variabile num non contiene un numero
+    console.log("non è un numero");
+
 
 } else {
-    console.log("you lose");
+    console.log("hai perso");
+    document.getElementById('lose');
+    element.setAttribute("class", "visible");
 }
