82f048d15f8e14c4ab7eaff0e835b02e1d4cc776
Fabienne <fortunio.fabienne@gmail.com>
2019-11-15 16:39:55 +0100

    funziona

--- game.js
+++ game.js
@@ -1,9 +1,14 @@
 /*Generare un numero casuale tra 1 e 6.
 Attraverso un prompt chiedere all'utente un numero tra 1 e 6.
-Stampare un messaggio per dire all'utente se ha vinto oppure no (il numero più alto vince).*/
+Stampare un messaggio per dire all'utente se ha vinto oppure no (il numero più alto vince).
+
+Obiettivo: genero numero casuale
+Punto 1:
+*/
 
 var num = prompt("Entry a number");
-var num2 = alert(Math.floor(Math.random() * 6));
+var num2 =(Math.floor(Math.random() * 5)+1);
+alert(num2); //come cambiare la scritta della alerta
 
 if (num > num2){
     console.log("you won");
